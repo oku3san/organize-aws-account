5418c239712a925398f1a8c3b517ab4a1eeb8ab2
oku3san <takaya0303@gmail.com>
2021-06-24 00:14:50 +0900

    Delete template for security hub

--- securityhub.tf
+++ securityhub.tf
@@ -34,12 +34,4 @@ resource "aws_cloudwatch_event_target" "securityhub" {
   arn = aws_sns_topic.chatbot.arn
   target_id = "securityhub"
   rule = aws_cloudwatch_event_rule.securityhub.name
-
-  input_transformer {
-    input_paths = {
-      "description" = "$.detail.findings[0].Description"
-      "severity" = "$.detail.findings[0].Severity.Label"
-    }
-    input_template = "\"Security Hub(<severity>) - <description>\""
-  }
 }
