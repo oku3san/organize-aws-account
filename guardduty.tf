df43a237140d1408167d8a7c47d0b481f2b4c05e
oku3san <takaya0303@gmail.com>
2021-06-23 23:53:52 +0900

    Delete settings of eventbridge to consolidate to security hub

--- guardduty.tf
+++ guardduty.tf
@@ -2,29 +2,3 @@ resource "aws_guardduty_detector" "default" {
   enable = true
   finding_publishing_frequency = "SIX_HOURS"
 }
-
-resource "aws_cloudwatch_event_rule" "guardduty" {
-  name = "guardduty"
-  event_pattern = jsonencode({
-    source = ["aws.guardduty"]
-    detail-type = ["GuardDuty Finding"]
-  })
-}
-
-resource "aws_cloudwatch_event_target" "guardduty" {
-  arn = aws_sns_topic.mail.arn
-  rule = aws_cloudwatch_event_rule.guardduty.name
-  target_id = "guardduty"
-
-  input_transformer {
-    input_paths = {
-      "type" = "$.detail.type"
-      "description" = "$.detail.description"
-      "severity" = "$.detail.severity"
-    }
-    input_template = <<EOF
-"You have a severity <severity> GuardDuty finding type <type>"
-"<description>"
-    EOF
-  }
-}
