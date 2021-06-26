a101beb44de2a004983ccb6d0636f7c20431dd1b
oku3san <takaya0303@gmail.com>
2021-06-26 16:32:10 +0900

    Change execute user

--- iam.tf
+++ iam.tf
@@ -1,23 +1,33 @@
-data "aws_iam_policy_document" "terraform_admin_access" {
+resource "aws_iam_group" "terraform_admin" {
+  name = "terraform_admin"
+}
+
+data "aws_iam_policy_document" "terraform_admin_sts_policy" {
   statement {
     effect    = "Allow"
-    actions   = ["*"]
+    actions   = ["sts:AssumeRole"]
     resources = ["*"]
   }
 }
 
-resource "aws_iam_policy" "terraform_admin_access" {
-  name   = "terraform_admin_access"
-  policy = data.aws_iam_policy_document.terraform_admin_access.json
+resource "aws_iam_policy" "terraform_admin_sts_policy" {
+  name   = "terraform_sts_policy"
+  policy = data.aws_iam_policy_document.terraform_admin_sts_policy.json
+}
+
+resource "aws_iam_group_policy_attachment" "terraform_admin_readonly" {
+  group      = aws_iam_group.terraform_admin.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "terraform_admin_stg" {
+  group      = aws_iam_group.terraform_admin.name
+  policy_arn = aws_iam_policy.terraform_admin_sts_policy.arn
 }
 
 resource "aws_iam_user" "terraform" {
   name          = "terraform"
   force_destroy = true
-}
-
-resource "aws_iam_group" "terraform_admin" {
-  name = "terraform_admin"
 }
 
 resource "aws_iam_group_membership" "terraform_admin" {
@@ -26,9 +36,38 @@ resource "aws_iam_group_membership" "terraform_admin" {
   users = [aws_iam_user.terraform.name]
 }
 
-resource "aws_iam_group_policy_attachment" "terraform_admin" {
-  group      = aws_iam_group.terraform_admin.name
-  policy_arn = aws_iam_policy.terraform_admin_access.arn
+data "aws_iam_policy_document" "terraform_role_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "terraform" {
+  name   = "terraform_admin_policy"
+  policy = data.aws_iam_policy_document.terraform_role_policy.json
+}
+
+data "aws_iam_policy_document" "terraform_role_assume_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${aws_iam_user.terraform.name}"]
+    }
+  }
+}
+
+resource "aws_iam_role" "terraform" {
+  name               = "terraform"
+  assume_role_policy = data.aws_iam_policy_document.terraform_role_assume_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "terraform" {
+  policy_arn = aws_iam_policy.terraform.arn
+  role       = aws_iam_role.terraform.name
 }
 
 resource "aws_iam_account_password_policy" "strict" {
--- main.tf
+++ main.tf
@@ -7,14 +7,19 @@ terraform {
   }
 
   backend "s3" {
-    region = "ap-northeast-1"
-    bucket = "oku3san-tfstate-bucket"
-    key    = "organize-aws-account-tfstate"
-
+    region         = "ap-northeast-1"
+    bucket         = "oku3san-tfstate-bucket"
+    key            = "organize-aws-account-tfstate"
     dynamodb_table = "tfstate_lock"
+    role_arn       = "arn:aws:iam::950007738962:role/terraform"
   }
 }
 
 provider "aws" {
   region = "ap-northeast-1"
+  assume_role {
+    role_arn = "arn:aws:iam::950007738962:role/terraform"
+  }
 }
+
+data "aws_caller_identity" "current" {}
