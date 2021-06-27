c251d1aad768ac82599a6eebf1249d4e12f34f1b
oku3san <takaya0303@gmail.com>
2021-06-27 21:29:06 +0900

    Enable kms encrypt for CloudTrail

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -59,6 +59,134 @@ module "cloudtrail_iam_role" {
   policy     = data.aws_iam_policy_document.cloudtrail.json
 }
 
+data "aws_iam_policy_document" "cloudtrail_kms" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/terraform",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/terraform",
+        "arn:aws:sts::${data.aws_caller_identity.current.account_id}:federated-user/oku3san",
+        "arn:aws:sts::${data.aws_caller_identity.current.account_id}:federated-user/terraform",
+      ]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["cloudtrail.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = [
+      "kms:GenerateDataKey*"
+    ]
+    condition {
+      test     = "StringLike"
+      values   = ["arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+    }
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["cloudtrail.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = [
+      "kms:DescribeKey"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:ReEncryptFrom"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      values   = [data.aws_caller_identity.current.account_id]
+      variable = "kms:CallerAccount"
+    }
+    condition {
+      test     = "StringLike"
+      values   = ["arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    actions = [
+      "kms:CreateAlias"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      values   = ["ec2.${data.aws_region.current.name}.amazonaws.com"]
+      variable = "kms:ViaService"
+    }
+    condition {
+      test     = "StringEquals"
+      values   = [data.aws_caller_identity.current.account_id]
+      variable = "kms:CallerAccount"
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:ReEncryptFrom"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      values   = [data.aws_caller_identity.current.account_id]
+      variable = "kms:CallerAccount"
+    }
+    condition {
+      test     = "StringLike"
+      values   = ["arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+    }
+  }
+}
+
+resource "aws_kms_key" "cloudtrail" {
+  description             = "Master Key for cloudtrail"
+  enable_key_rotation     = true
+  is_enabled              = true
+  deletion_window_in_days = 30
+  policy                  = data.aws_iam_policy_document.cloudtrail_kms.json
+}
+
+resource "aws_kms_alias" "cloudtrail" {
+  name          = "alias/cloudtrail"
+  target_key_id = aws_kms_key.cloudtrail.key_id
+}
+
 resource "aws_cloudtrail" "default" {
   name                          = "default"
   s3_bucket_name                = module.cloudtrail_log_bucket.name
@@ -68,5 +196,6 @@ resource "aws_cloudtrail" "default" {
   enable_log_file_validation    = true
   cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.logs.arn}:*"
   cloud_watch_logs_role_arn     = module.cloudtrail_iam_role.arn
+  kms_key_id                    = aws_kms_alias.cloudtrail.arn
   depends_on                    = [aws_s3_bucket_policy.cloudtrail_log]
 }
--- main.tf
+++ main.tf
@@ -23,3 +23,4 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
