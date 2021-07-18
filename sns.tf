cc900ee21df7260294348d46b4beaa23682eb1b8
oku3san <takaya0303@gmail.com>
2021-07-18 22:15:41 +0900

    Change sns topic

--- sns.tf
+++ sns.tf
@@ -1,9 +1,9 @@
 resource "aws_sns_topic" "mail" {
-  name              = "alert-mail"
+  name              = "mail"
   kms_master_key_id = "alias/aws/sns"
 }
 
-data "aws_iam_policy_document" "cloudwatch_events" {
+data "aws_iam_policy_document" "mail" {
   statement {
     effect    = "Allow"
     resources = [aws_sns_topic.mail.arn]
@@ -12,19 +12,20 @@ data "aws_iam_policy_document" "cloudwatch_events" {
     principals {
       identifiers = [
         "cloudwatch.amazonaws.com",
-        "events.amazonaws.com"
+        "events.amazonaws.com",
+        "budgets.amazonaws.com"
       ]
       type = "Service"
     }
   }
 }
 
-resource "aws_sns_topic_policy" "cloudwatch_events" {
+resource "aws_sns_topic_policy" "mail" {
   arn    = aws_sns_topic.mail.arn
-  policy = data.aws_iam_policy_document.cloudwatch_events.json
+  policy = data.aws_iam_policy_document.mail.json
 }
 
-resource "aws_sns_topic_subscription" "mail_subscription" {
+resource "aws_sns_topic_subscription" "mail" {
   topic_arn = aws_sns_topic.mail.arn
   protocol  = "email"
   endpoint  = aws_ssm_parameter.email.value
