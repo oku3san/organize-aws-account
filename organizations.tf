26d342f956bb664aa477532625748b0dbfa78383
oku3san <takaya0303@gmail.com>
2021-08-09 01:56:33 +0900

    Enable SSO

--- organizations.tf
+++ organizations.tf
@@ -1,7 +1,8 @@
 resource "aws_organizations_organization" "organizations" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
-    "config.amazonaws.com"
+    "config.amazonaws.com",
+    "sso.amazonaws.com"
   ]
   feature_set = "ALL"
 }
